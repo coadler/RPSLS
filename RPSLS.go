cbfa8f835318b5adf6ac46621515b9b57851b54c
ThyLeader <colin@cadler.co>
2016-03-11 08:57:12 -0500

    Fixed tabs accidentally copy and pasted when switching text editors

--- RPSLS.go
+++ RPSLS.go
@@ -31,134 +31,122 @@ ______           _     ______                       _____      _                
 package main
 
 import ("fmt"
-		"math/rand"
-		"time"
-		//"net/http"
+        "math/rand"
+        "time"
+        //"net/http"
 )
 var patterns = []string{"NULL", "Scissors","Paper", "Rock", "Lizard", "Spock"}
 
 func main(){
-	done := false
-	for done == false{
-		fmt.Println("\n\tWelcome to RPSLS")
-		fmt.Println("Please choose one of the following:\n")
-		fmt.Println("1.\tPlay")
-		fmt.Println("0.\tExit Game\n")
-		fmt.Print("Please select a game mode: ")
-		var mode int;
-		fmt.Scanf("%d", &mode);
-		switch mode {
-		default: fmt.Println("\nError: Please choose 0 or 1\n\n")
-		case 0: done = true			// bie bie
-		case 1: ai()
-		}
-	}
+        done := false
+        for done == false{
+        fmt.Println("\n\tWelcome to RPSLS")
+        fmt.Println("Please choose one of the following:\n")
+        fmt.Println("1.\tPlay")
+        fmt.Println("0.\tExit Game\n")
+        fmt.Print("Please select a game mode: ")
+        var mode int;
+        fmt.Scanf("%d", &mode);
+        switch mode {
+            default: fmt.Println("\nError: Please choose 0 or 1\n\n")
+            case 0: done = true			// bie bie
+            case 1: ai()
+        }
+    }
 }
 func printSelections(){
-	fmt.Println("\n===========================")
-	fmt.Println("\tSelections")
-	fmt.Println("===========================\n")
-	fmt.Println("1. Scissors")
-	fmt.Println("2. Paper")
-	fmt.Println("3. Rock")
-	fmt.Println("4. Lizard")
-	fmt.Println("5. Spock\n")
+    fmt.Println("\n===========================")
+    fmt.Println("\tSelections")
+    fmt.Println("===========================\n")
+    fmt.Println("1. Scissors")
+    fmt.Println("2. Paper")
+    fmt.Println("3. Rock")
+    fmt.Println("4. Lizard")
+    fmt.Println("5. Spock\n")
 }
 func ai(){
-	var p1, p2, result int
-	r := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))	// makes a rand instance
-	// ask for input
-	printSelections()
-	fmt.Print("Please enter your choice: ")
-	fmt.Scanf("%d", &p1)
+    var p1, p2, result int
+    r := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))	// makes a rand instance
+    // ask for input
+    printSelections()
+    fmt.Print("Please enter your choice: ")
+    fmt.Scanf("%d", &p1)
 
-	n, err := fmt.Scanf("%f\n", &inputSquare)
+    n, err := fmt.Scanf("%f\n", &inputSquare)
     if err != nil || n != 1 {
         // handle invalid input
         fmt.Println(n, err)
         return
     }
 	
-	// make ai's choice
-	p2 = r.Intn(5) + 1;
-	fmt.Println("\nThe computer's choice is", patterns[p2], "\n")
-	// Judge
-	result = judge(p1, p2, true)
-	// Print
-	switch result{
-	case -1: fmt.Println("Computer wins\n")
-	case 0 : fmt.Println("Tie\n")
-	case 1, 2, 3, 4 : fmt.Println("You win!\n")
-	}
+    // make ai's choice
+    p2 = r.Intn(5) + 1;
+    fmt.Println("\nThe computer's choice is", patterns[p2], "\n")
+    // Judge
+    result = judge(p1, p2, true)
+    // Print
+    switch result {
+    case -1: fmt.Println("Computer wins\n")
+    case 0 : fmt.Println("Tie\n")
+    case 1, 2, 3, 4 : fmt.Println("You win!\n")
+    }
 }
-func judge(p1 int, p2 int, displayR bool) int{
+func judge(p1 int, p2 int, displayR bool) int {
 	
-	var ret	int;
-	death := "breaks the code of"
-	// determine result
-	switch (p1 - p2){
-	default: 		ret = -1	// p1 lose
-	case 0:			ret = 0		// tie
-	case -1:		ret = 1		// p1 win
-		//fmt.Println("this is case -1")	//scissors -> paper | paper -> rock | rock -> lizard | lizard -> spock
-	case -3:		ret = 2		// p1 win
-		//fmt.Println("this is case -3") 	//scissors -> lizard | paper -> rock
-	case 2:			ret = 3		// p1 win
-		//fmt.Println("this is case -2")	//spock -> rock | rock -> scissors | lizard -> paper
-	case 4:			ret = 4		// p1 win
-		//fmt.Println("this is case -4") 	// spock -> scissors
-	}
-	// display result
-	if displayR == true {
-		switch (ret) {
-		default:
-			fmt.Println("rip code")			// congrats if u get this
-
-		case -1:
-			death = "is beaten by"			// lose
-
-		case 0:
-			death = "ties with"				// tie
-
-		case 1: 		
-			if p1 == 1 {
-				death = "cuts"				// scissors -> paper
-			}
-			if p1 == 2 {
-				death = "covers"			// paper -> rock
-			}
-			if p1 == 3 {
-				death = "crushes"			// rock -> lizard
-			}
-			if p1 == 5 {
-				death = "disproves"			// lizard -> spock
-			}
-
-		case 2: 		
-			if p1 == 1 {
-				death = "decapitate"		// scissors -> lizard
-			}
-			if p1 == 2 {
-				death = "covers"			// paper -> rock
-			}
-
-		case 3: 		
-			if p1 == 5 {
-				death = "vaporizes"			// spock -> rock
-			}
-			if p1 == 3 {
-				death = "crushes"			// rock -> scissors
-			}
-			if p1 == 4 {
-				death = "eats"				// lizard -> paper
-			}
-
-		case 4:
-			death = "smashes"				// spock -> scissors
-		}
-	}
-	fmt.Println(patterns[p1], death, patterns[p2], "\n")
-	return ret
+    var ret	int;
+    death := "breaks the code of"
+    // determine result
+    switch (p1 - p2){
+    default:        ret = -     // p1 lose
+    case 0:         ret = 0     // tie
+    case -1:        ret = 1     // p1 win | scissors -> paper | paper -> rock | rock -> lizard | lizard -> spock
+    case -3:        ret = 2     // p1 win | scissors -> lizard | paper -> rock
+    case 2:         ret = 3     // p1 win | spock -> rock | rock -> scissors | lizard -> paper
+    case 4:         ret = 4     // p1 win | spock -> scissors
+    }
+    // determine how the computer dies
+    if displayR == true {
+        switch (ret) {
+        default:
+            fmt.Println("rip code")         // congrats if u get this
+        case -1:
+            death = "is beaten by"          // lose
+        case 0:
+            death = "ties with"				// tie
+        case 1: 		
+            if p1 == 1 {
+                death = "cuts"              // scissors -> paper
+            }
+            if p1 == 2 {
+                death = "covers"            // paper -> rock
+            }
+            if p1 == 3 {
+                death = "crushes"           // rock -> lizard
+            }
+            if p1 == 5 {
+                death = "disproves"         // lizard -> spock
+            }
+        case 2: 		
+            if p1 == 1 {
+                death = "decapitate"        // scissors -> lizard
+            }
+            if p1 == 2 {
+                death = "covers"            // paper -> rock
+            }
+        case 3: 		
+            if p1 == 5 {
+                death = "vaporizes"         // spock -> rock
+            }
+            if p1 == 3 {
+                death = "crushes"           // rock -> scissors
+            }
+            if p1 == 4 {
+                death = "eats"              // lizard -> paper
+            }
+        case 4:
+            death = "smashes"               // spock -> scissors
+        }
+    }
+    fmt.Println(patterns[p1], death, patterns[p2], "\n")
+    return ret
 }
-
-// congrats you read my code
